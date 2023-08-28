a391e664b82b2850d03b8a9a38a2b03d4a3d9272
abhishekwin <abhishek@thoughtwin.com>
2023-08-28 11:02:06 +0530

    eleven

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const port = 3000;
 const app = express();
+const router = require("./src/router/index");
 
 // Body parse
 
@@ -10,7 +11,7 @@ const db = require("./src/models/index");
 const startServer = async () => {
   try {
     await db.authenticate();
-    app.use("/api", router);
+    app.use("/api", router); 
 
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
